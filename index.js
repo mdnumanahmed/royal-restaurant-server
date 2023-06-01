e278bf3513330ad6c48a745eb77ee940623f1e75
mdnumanahmed <mnumansyl@gmail.com>
2023-06-02 01:42:20 +0600

    get api for carts by email added

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ async function run() {
     })
 
     // carts collection ali
+    app.get('/carts', async(req,res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([])
+      }
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+  
     app.post('/carts', async(req, res) => {
       const item = req.body;
       const result = await cartCollection.insertOne(item)
